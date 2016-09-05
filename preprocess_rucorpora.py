ad7b6fd1dece4cea30d2a32550592a9d909dd3aa
Valentin Malykh <valentin@maly.hk>
2016-09-05 19:35:03 +0300

    ENH: new kewl parsing

--- preprocess_rucorpora.py
+++ preprocess_rucorpora.py
@@ -1,16 +1,17 @@
-import BeautifulSoup as bs
+import bs4 as bs
 import os
 import codecs
+from tqdm import tqdm
 
 tasks = []
 for root, dirs, fs in os.walk("rucorpora"):
     tasks.extend([os.path.join(root, f) for f in fs if f[-6:] == '.xhtml'])
 
 all_data = ""
-for f in tasks:
+for f in tqdm(tasks):
     with codecs.open(f, encoding="cp1251") as f_in:
         f_in.readline()
-        soup = bs.BeautifulSoup(f_in.read())
-        all_data += soup.getText()
+        soup = bs.BeautifulSoup(f_in.read(), "lxml")
+        all_data += soup.getText().replace("`", "").replace("\n", " ")
 
-print all_data
+print " ".join(all_data)
